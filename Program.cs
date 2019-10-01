793e0bfeeca1304e43c495be4cd233ae467b7e35
Maximiliam02 <54435275+Maximiliam02@users.noreply.github.com>
2019-10-01 13:49:56 +0200

    Första Array uppgiften
    Gjorde första Array uppgiften

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv 5 heltal");
+            int[] Tal = new int[5];
+            for (int i = 0; i < Tal.GetLength(0); i++)
+                {
+                string m = Console.ReadLine();
+                int.TryParse(m, out int a);
+                Tal[i] = a;                
+            }
+            
+            for (int i = 4; i > -1; i--)
+            {
+                Console.WriteLine(Tal[i]);
+            }
+
+
+
+        }
+        
+    }
+}
